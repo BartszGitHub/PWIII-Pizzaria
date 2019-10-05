edd1aa1a54db4fef6921b93d203fa977565c1b29
Bruno e Igor <brunohma@live.com>
2019-10-04 22:24:11 -0300

    exercicio 8, 9 e 10

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -22,4 +22,31 @@ export class ComandaComponent implements OnInit {
     });
   }
 
+  OnProcessar() {
+    let chopp = this.formPizzaria.value.chopp;
+
+    let pizzas = this.formPizzaria.value.pizzas;
+
+    let recheios = this.formPizzaria.value.recheios;
+
+    let pessoas = this.formPizzaria.value.pessoas;
+
+    let servico = this.formPizzaria.value.servico;
+
+    let consumo = (chopp * 7.30) + (pizzas * 31.50) + (recheios * 5.90);
+    let taxa = consumo * 0.10;
+    let total = consumo;
+    
+
+    if (servico)
+    {
+       total = consumo + taxa;
+    }
+    
+    let consumo_pessoa = total / pessoas;
+
+    alert(`Consumo: R$ ${consumo.toFixed(2)} \n Serviços: R$ ${taxa.toFixed(2)} \n Total: R$ ${total.toFixed(2)} \n Por pessoa: R$ ${consumo_pessoa.toFixed(2)}`);
+
+  }
+
 }
